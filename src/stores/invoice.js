d81a298c1709e59903669b23ff7c375691ecfd52
zhuzhimeng <815259837@qq.com>
2024-12-06 10:48:47 +0800

    bolt提交的修改

---
+++ src/stores/invoice.js
@@ -0,0 +1,33 @@
+import { ref } from 'vue'
+
+export const invoiceData = ref([
+  {
+    id: 1,
+    type: '增值税电子普通发票',
+    seller: '武汉京东世纪贸易有限公司',
+    buyer: '武汉数元科技有限公司',
+    date: '2023年11月05日',
+    amount: '19.86',
+    invoiceCode: '042002300411',
+    invoiceNumber: '20974350',
+    verificationCode: '63587666250261362468',
+    items: [
+      {
+        name: '移动通信设备·通讯器材及配件',
+        amount: '-0.92',
+        taxRate: '13%',
+        tax: '-0.12'
+      },
+      {
+        name: '移动通信设备·通讯器材及配件',
+        amount: '18.50',
+        taxRate: '13%',
+        tax: '2.4'
+      }
+    ]
+  }
+])
+
+export function getInvoiceById(id) {
+  return invoiceData.value.find(invoice => invoice.id === parseInt(id))
+}
---
+++ src/views/InvoicesView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import InvoiceList from '../components/InvoiceList.vue'
+
+
+const noMoreData = ref(true)
+</script>
+
+<template>
+  <div>
+    <header class="page-header">
+      <van-nav-bar title="票夹" />
+    </header>
+    <invoice-list />
+  </div>
+</template>
+
